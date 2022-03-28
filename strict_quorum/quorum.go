d8ae86bada7951eee4a6d6bf0a2742931b0a35d8
Filbert Cia <cia.filbert@gmail.com>
2022-03-28 22:49:07 +0800

    modified quorum.go

--- strict_quorum/quorum.go
+++ strict_quorum/quorum.go
@@ -1,31 +1,49 @@
 package strict_quorum
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"net/http"
+	"math"
+	"time"
 )
 
-func (h *Handler) HandleQuorumRequest(c *fiber.Ctx) error {
-	var (
-		requestMsg PeerMessage
-	)
-	quorumResponse := &PeerMessage{
-		Type:     QUORUM_OK,
-		Content:  "1",
-		SourceID: h.Node.Id,
-	}
-	err := c.BodyParser(&requestMsg)
-	if err != nil {
-		return err
-	}
-	resp, err := json.Marshal(quorumResponse)
-	if err != nil {
-		_ = c.SendStatus(http.StatusInternalServerError)
-		return err
-	}
-	_ = c.Send(resp)
-	fmt.Printf("Quorum OK sent to Node: %d\n", requestMsg.SourceID)
+func (h *Handler) createQuorum() error {
+	h.Responses = 0
+	h.Ring.MinVotes = int(math.Ceil(float64(h.Ring.ReplicationFactor / 2)))
+	quorumChannel := make(chan PeerMessage)
+	h.QuorumChannel = quorumChannel
+	go h.collectReplies()
 	return nil
+}
+
+func (h *Handler) closeQuorum() error {
+	defer close(h.QuorumChannel)
+	time.Sleep(h.Timeout)
+	if h.Responses >= h.Ring.MinVotes {
+		fmt.Println("Quorum passed!")
+		fmt.Printf("Node %d: Number of votes received: %d\tNumber of votes required:%d\n", h.Node.Id, h.Responses, h.Ring.MinVotes)
+		return nil
+	} else {
+		fmt.Println("Insufficient Quorum")
+		fmt.Printf("Node %d: Number of votes received: %d\tNumber of votes required:%d\n", h.Node.Id, h.Responses, h.Ring.MinVotes)
+		return errors.New("write failed: insufficient ACKs")
+	}
+}
+
+func (h *Handler) collectReplies() {
+	//TODO: channel should close and node should move on once quorum is reached
+	node := h.Node
+	for {
+		select {
+		case reply := <-h.QuorumChannel:
+			if reply.Type == READ_OK {
+				node.DataStore[reply.SourceID] = reply
+			}
+			h.Responses++
+			fmt.Printf("Data received. Current ACKs: %d\n", h.Responses)
+		case <-time.After(h.Timeout):
+			fmt.Printf("\nTimeout\n")
+			return
+		}
+	}
 }
