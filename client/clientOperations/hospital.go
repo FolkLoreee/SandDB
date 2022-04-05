f8986d94c8f1b53308f5ef71e5a2df93ca7bdcd6
zhiyi <ongzhiyi@live.com>
2022-04-05 11:53:09 +0800

    tidied up base code

--- client/clientOperations/hospital.go
+++ client/clientOperations/hospital.go
@@ -7,11 +7,7 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
-
-	// "reflect"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -33,40 +29,10 @@ type Request struct {
 	SourceID int         `json:"node_id"`
 }
 
-type Hospitals struct {
-	Hospitals []Hospital `json:"hospitals"`
-}
-
-type Hospital struct {
-	Partition Partition `json:"partition"`
-	Rows      []Row     `json:"rows"`
-}
-
-type Partition struct {
-	Key      []string `json:"key"`
-	Position int      `json:"position"`
-}
-
-type Row struct {
-	Type          string              `json:"type"`
-	Position      int                 `json:"position"`
-	Clustering    []string            `json:"clustering"`
-	LivelinesInfo Liveliness          `json:"liveliness_info"`
-	DeletionInfo  map[string]int      `json:"deletion_info"`
-	Cells         []map[string]string `json:"cells"`
-}
-
-type Liveliness struct {
-	Tstamp  int  `json:"tstamp"`
-	TTL     int  `json:"ttl"`
-	Expires int  `json:"expires_at"`
-	Expired bool `json:"expired"`
-}
-
 func GetAllHospitals(c *fiber.Ctx) error {
 	var reply interface{}
 
-	fmt.Println("Received request")
+	fmt.Println("Requesting for all hospital resources")
 
 	jsonResponse, err := sendReadRequest(c)
 	err = json.Unmarshal([]byte(jsonResponse), &reply)
@@ -80,24 +46,6 @@ func GetAllHospitals(c *fiber.Ctx) error {
 
 func GetHospital(c *fiber.Ctx) error {
 	var hospitalData []map[string]string
-
-	// Open our jsonFile
-	hospitals, err := readJSON("data.json")
-	if err != nil {
-		fmt.Printf("Error reading JSON file: %s", err.Error())
-	}
-
-	hospitalId, _ := strconv.Atoi(c.Params("hospitalID"))
-	fmt.Printf("Getting information for hospital %d...", hospitalId)
-	hospital := hospitals.Hospitals[hospitalId]
-
-	for _, row := range hospital.Rows {
-		data := row.Cells
-		for _, cell := range data {
-			hospitalData = append(hospitalData, cell)
-		}
-	}
-
 	return c.JSON(fiber.Map{"status": "success", "message": hospitalData})
 }
 
@@ -107,22 +55,6 @@ func GetResource(c *fiber.Ctx) error {
 }
 
 // helper function
-func readJSON(filename string) (Hospitals, error) {
-	jsonFile, err := os.Open(filename)
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		return Hospitals{}, err
-	}
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var hospitals Hospitals
-	json.Unmarshal([]byte(byteValue), &hospitals)
-
-	return hospitals, nil
-}
-
 func sendReadRequest(c *fiber.Ctx) ([]byte, error) {
 	request := Request{
 		Type:    REQUEST_READ,
