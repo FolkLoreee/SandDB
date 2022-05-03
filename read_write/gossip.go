e32c1353d77fa9fda43e67cf18bf578b25b3afb8
zhiyi <ongzhiyi@live.com>
2022-05-03 18:35:52 +0800

    client ver 1

---
+++ read_write/gossip.go
@@ -0,0 +1,25 @@
+package read_write
+
+// import (
+// 	"fmt"
+// )
+
+// const NUM_NODES = 5
+
+// /*
+// gossip protocol:
+// - each node initiates a gossip every few seconds, chooses 3 nodes randomly to initiate with, sending all the data that it stored + own data
+// - node checks received data against own data, updates own data if necessary and replies with its stored data about other nodes + its own data
+// - data consists of when the node was started + time of gossip
+// */
+
+// func (node *Handler) startGossip() {
+
+// }
+
+// func main() {
+// 	fmt.Println("hewo")
+// 	for i := 0; i < NUM_NODES; i++ {
+// 		node := Node{}
+// 	}
+// }
